af67a4c0757a9385c220ac9abc7b39593826d3b4
MeritcoLiang <15311775795@163.com>
2026-07-08 12:52:39 +0000

    Make WebSocket event sink thread-safe

--- src/tsgo/web/event_bus.py
+++ src/tsgo/web/event_bus.py
@@ -10,9 +10,10 @@ from ..events import TraceEvent
 
 @dataclass(slots=True)
 class AsyncQueueEventSink:
-    """Synchronous EventSink implementation backed by an asyncio.Queue."""
+    """Thread-safe bridge from PipelineController events to an asyncio.Queue."""
 
     queue: asyncio.Queue[TraceEvent]
+    loop: asyncio.AbstractEventLoop
 
     def emit(self, event: TraceEvent) -> None:
-        self.queue.put_nowait(event)
+        self.loop.call_soon_threadsafe(self.queue.put_nowait, event)
