d99834f522f79eb75fedab36433ce7aa20057483
MeritcoLiang <15311775795@163.com>
2026-07-09 14:23:23 +0000

    Export SecondaryMarketAnalyst LLM operator set

--- src/tsgo/experts/__init__.py
+++ src/tsgo/experts/__init__.py
@@ -5,5 +5,6 @@ execution semantic beyond Operator.
 """
 
 from .secondary_market import build_secondary_market_operators
+from .secondary_market_llm import build_secondary_market_llm_operators
 
-__all__ = ["build_secondary_market_operators"]
+__all__ = ["build_secondary_market_operators", "build_secondary_market_llm_operators"]
